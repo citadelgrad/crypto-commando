51366fe534e6567eee7820423e3545ef57470520
Scott Nixon <citadelgrad@gmail.com>
2021-06-27 19:55:14 -0700

    Setup pytests.

---
+++ app/helpers.py
@@ -0,0 +1,13 @@
+from eth_account import Account
+import secrets
+
+
+def test_account():
+    """
+    Create a temp private key for testing.
+    """
+    priv = secrets.token_hex(32)
+    private_key = "0x" + priv
+    print("SAVE BUT DO NOT SHARE THIS:", private_key)
+    acct = Account.from_key(private_key)
+    print("Address:", acct.address)
