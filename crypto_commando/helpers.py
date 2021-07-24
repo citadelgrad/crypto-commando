94d1c6a17ebf840ee69563d709ca37a12b4b9587
Scott Nixon <citadelgrad@gmail.com>
2021-07-24 15:16:26 -0700

    Get ABI via etherscan

--- crypto_commando/helpers.py
+++ crypto_commando/helpers.py
@@ -1,5 +1,9 @@
+import json
+import requests
 from eth_account import Account
 import secrets
+
+from crypto_commando.config import ETHERSCAN_API_KEY
 
 
 def test_account():
@@ -11,3 +15,14 @@ def test_account():
     print("SAVE BUT DO NOT SHARE THIS:", private_key)
     acct = Account.from_key(private_key)
     print("Address:", acct.address)
+
+
+def get_contract_abi(contract):
+    response = requests.get(
+        f"https://api.etherscan.io/api?module=contract&action=getabi&address={contract}&apikey={ETHERSCAN_API_KEY}"
+    )
+    data = json.loads(response.text)
+    if data.get("message") == "OK":
+        return data.get("result")
+    else:
+        raise "Unable to get contract ABI"
